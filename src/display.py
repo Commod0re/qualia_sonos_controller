3dae9da67fe4f36edff897f7a26ead36134afe0b
Michael Greene <michael.greene@pyrodyne.org>
2024-06-16 16:40:41 -0700

    rotate display to prepare for case

--- src/display.py
+++ src/display.py
@@ -3,5 +3,5 @@ from adafruit_qualia import Qualia
 from adafruit_qualia.graphics import Displays
 
 displayio.release_displays()
-qualia = Qualia(Displays.SQUARE40)
+qualia = Qualia(Displays.SQUARE40, rotation=90)
 qualia.display.root_group = displayio.CIRCUITPYTHON_TERMINAL
