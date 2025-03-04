bb1444f3f4871a539d245afcea01c4245b95807f
Michael Greene <a.minor.threat@gmail.com>
2025-03-04 12:52:54 -0800

    show current volume too

--- src/ui/widgets/volume.py
+++ src/ui/widgets/volume.py
@@ -1,3 +1,5 @@
+import terminalio
+from adafruit_display_text import label
 from .slider import Slider
 
 
@@ -8,8 +10,15 @@ class Volume(Slider):
 
     @volume.setter
     def volume(self, new_vol):
-        print(f'{new_vol=} {new_vol/100}')
+        self._vol_label.text = f'{new_vol}'
         self.position = new_vol / 100
 
     def __init__(self, *args, **kwargs):
         super().__init__(orientation='vertical', **kwargs)
+
+        self._vol_label = label.Label(terminalio.FONT, text='50', scale=2)
+        self._vol_label.anchor_point = (0.5, 0.5)
+        self._vol_label.anchored_position = (self.width // 2, self.height // 2)
+        self._vol_label._palette.make_opaque(0)
+
+        self.append(self._vol_label)
